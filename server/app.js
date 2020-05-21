36737bd6b4f27a6eaef3e69d275ff5796c1560bd
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-21 01:25:11 +0100

    Students can be added. Can currently only add the name. Will later return to this to enable email and degree to be added

--- server/app.js
+++ server/app.js
@@ -6,6 +6,26 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+app.post("/students", async (req, res) => {
+    try {
+        console.log(req.body);
+        const { student_name } = req.body;
+        const { student_email } = req.body;
+        const { degree_course } = req.body;
+        const newStudent = await pool.query(
+          "INSERT INTO students (student_name) VALUES($1)  RETURNING *", 
+          [student_name]
+        );
+      
+        res.json(newStudent.rows[0]);
+  
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+
+
 
 app.listen(5000, function() {
     console.log("Listening for requests on Port 5000")

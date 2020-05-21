21014604349c6707b0d5b8083830bf0164748754
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-21 12:55:57 +0100

    Input component is being rendered correctly and the database can be updated using Postman. However, data from the form is not being added currently to the database

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Input />
     </div>
   </Fragment>
-  )
+  );
 }
        
 
--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,11 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Input = () => {
+
+    const [studentInformation, setStudentInformation] = useState("");
+
+    const onSubmitForm = async (event) => {
+        event.preventDefault(); 
+      try {
+          const body = { studentInformation };
+          const response =  await fetch("http://localhost:5000/students", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
+          });
+
+          console.log(response);
+
+
+         
+      } catch (err) {
+          console.error(err.message)
+      }
+    }
+
   return (
   <Fragment>
       <h3>Student List</h3>
-      <form>
-          <input type="text"></input> 
+      <form onSubmit={onSubmitForm} >
+          <input type="text" value={studentInformation} 
+          onChange={(event) => setStudentInformation(event.target.value)}>
+              </input> 
           <button>Add Student</button>
       </form>
   </Fragment>
